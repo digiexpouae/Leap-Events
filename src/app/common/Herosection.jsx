c1e1ff45b5e3695a7abf484d57a6a84c9a7f805f
digiexpouae <tech.digiexpo@gmail.com>
2026-03-11 09:42:04 +0500

    Fix:Herosection fix

--- src/app/common/Herosection.jsx
+++ src/app/common/Herosection.jsx
@@ -1,18 +1,36 @@
 export default function OurStorySection({ heading, text, className }) {
     return (
         <section className="w-full h-screen flex items-center justify-center bg-white  px-4 md:px-0">
-            <div className="max-w-5xl mx-auto  pt-20 ">
+            <div className="max-w-5xl mx-auto md:!hidden  flex  flex-col justify-between h-[40%] translate-y-[30%]  pt-20 ">
 
                 {/* Vision paragraph — right half */}
                 <div className="flex justify-end mb-6 md:mb-8">
-                    <p className="w-2/3  flex justify-end  text-sm md:text-[25px] tracking-tighter leading-[1.2] text-gray  "  >
+                    <p className="w-full md:w-2/3  flex md:justify-end  text-[25px] tracking-tighter leading-[1.2] text-gray  "  >
                         {text}
                     </p>
                 </div>
 
                 {/* OUR STORY heading */}
                 <h2
-                    className={`text-[clamp(3.5rem,14vw,9.5rem)] font-black uppercase leading-none text-center tracking-tight ${className}`}
+                    className={`text-[clamp(3.5rem,15vw,9.5rem)] font-black uppercase leading-none text-center tracking-tight ${className}`}
+                    style={{ color: "var(--color-heading)" }}
+                >
+                    {heading}
+                </h2>
+
+            </div>
+            <div className="max-w-5xl mx-auto hidden md:block pt-20 ">
+
+                {/* Vision paragraph — right half */}
+                <div className="flex justify-end mb-6 md:mb-8">
+                    <p className="w-full md:w-2/3  flex md:justify-end  text-[25px] tracking-tighter leading-[1.2] text-gray  "  >
+                        {text}
+                    </p>
+                </div>
+
+                {/* OUR STORY heading */}
+                <h2
+                    className={`text-[clamp(3.5rem,15vw,9.5rem)] font-black uppercase leading-none text-center tracking-tight ${className}`}
                     style={{ color: "var(--color-heading)" }}
                 >
                     {heading}
